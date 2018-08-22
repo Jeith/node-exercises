57391666021d5d39296e9fe3e2b3895052603383
Jeith <keithp1208@yahoo.com>
2018-08-22 15:56:20 -0500

    adding music database

---
+++ sql/.history/promise exercises/app_20180822114112.js
@@ -0,0 +1,24 @@
+var rp = require('request-promise');
+
+var urls = [ 'https://en.wikipedia.org/wiki/Futures_and_promises', 'https://en.wikipedia.org/wiki/Continuation-passing_style', 'https://en.wikipedia.org/wiki/JavaScript', 'https://en.wikipedia.org/wiki/Node.js', 'https://en.wikipedia.org/wiki/Google_Chrome' ];
+
+var cheerio = require('cheerio');
+ 
+var options = {
+    uri: 'http://www.google.com',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
+ 
+rp(options)
+    .then(function ($) {
+        
+    })
+    .catch(function (err) {
+        console.log("oops")
+    });
+
+cheerio.listen(8000, function () {
+    console.log('Listening on port 8000');
+});
