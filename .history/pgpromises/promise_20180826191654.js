326dd6284f5bcc275ab425dd97e1035519dfb765
Jeith <keithp1208@yahoo.com>
2018-08-26 19:38:53 -0500

    pgpromises

---
+++ .history/pgpromises/promise_20180826191654.js
@@ -0,0 +1,112 @@
+let promise = require('bluebird')
+const initOptions = {
+    promiseLib: promise, 
+};
+const pgp = require('pg-promise')(initOptions);
+var prompt = require('prompt-promise');
+
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'music_db',
+    user: 'postgres'
+};
+
+const db = pgp(config);
+
+albumArray = [];
+
+function albumName(){
+    prompt('Album name? ')
+    .then(function album(val) {
+        albumArray.push(val);
+        return prompt('Album year? ');
+    })
+    .then(function year(val) {
+        albumArray.push(val);
+        return prompt('Artist id? ');
+    })
+    .then(function id(val) {
+        albumArray.push(val);
+        console.log(albumArray);
+        prompt.done();
+        db.result(`INSERT INTO album \
+        VALUES (Default, '${albumArray[0]}', '${albumArray[1]}', '${albumArray[2]}')`)
+        .then(function (result) {
+            db.query(`select id from album order by id desc limit 1`)
+            .then(function(index){
+            console.log(`created album with id ${index[0].id}.`);
+            });
+        });
+    })
+    .catch(function rejected(err) {
+    console.log('error:', err.stack);
+    prompt.finish();
+    });
+};
+
+
+artistArray = [];
+
+function newArtist(){
+    prompt('Artist name? ')
+    .then(function artist(val) {
+        artistArray.push(val);
+        console.log(artistArray);
+        db.result(`INSERT INTO artist VALUES (Default, '${artistArray[0]}')`)
+        .then(function (result) {
+            db.query(`select id from artist order by id desc limit 1`)
+            .then(function(index){
+            console.log(`created artist with id ${index[0].id}.`);
+            });
+        })
+        .catch(function (err) {
+            console.log('error:', err.stack);
+        });
+        prompt.done();
+    })
+    
+    .catch(function rejected(err) {
+    console.log('error:', err.stack);
+    prompt.finish();
+    });
+};
+
+trackArray = [];
+
+function createTrack(){
+    prompt('Track name? ')
+    .then(function track(val) {
+        trackArray.push(val);
+        return prompt('Track Duration? ');
+    })
+    .then(function duration(val) {
+        trackArray.push(val);
+        return prompt('Album ID? ');
+    })
+    .then(function id(val) {
+        trackArray.push(val);
+        console.log(trackArray);
+        prompt.done();
+        db.result(`INSERT INTO track VALUES (Default, '${trackArray[0]}', '${trackArray[1]}', ${trackArray[2]})`)
+        .then(function (result) {
+        db.query(`select id from track order by id desc limit 1`)
+            .then(function(index){
+            console.log(`created track with id ${index[0].id}.`);
+            })
+        });
+    })
+    .catch(function rejected(err) {
+    console.log('error:', err.stack);
+    prompt.finish();
+    });
+};
+
+
+
+// albumName();
+
+// newArtist();
+
+createTrack();
--- pgpromises/routes/album.js
+++
@@ -1,4 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let bodyParser = require('body-parser');
-
